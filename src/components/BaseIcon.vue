baa7500389c1a7bf70aef22d76467253bea6409d
Vjacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2022-11-01 21:10:03 +0200

    base components were created

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    :class="classes"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    v-html="path"
+  ></svg>
+</template>
+
+<script>
+import icons from '../icons'
+export default {
+  props: ['name', 'classes'],
+
+  data() {
+    return {
+      path: icons[this.name],
+    }
+  },
+}
+</script>
